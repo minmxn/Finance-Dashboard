b1ae4b79e77535d7ca09da4fd9ccdf94d4bc1ee2
minmxn <72003734+minmxn@users.noreply.github.com>
2023-11-03 22:35:04 +0800

    print hi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { createTheme } from "@mui/material/styles"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+
 function App() {
-  return <div className="app"></div>
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return (
+  <div className="app">
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    hi
+  </ThemeProvider>
+  </div>
+  );
 }
 
 export default App
