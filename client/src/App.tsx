9f0423fb8bd9403e2d144046f30d541cf3188cbf
minmxn <72003734+minmxn@users.noreply.github.com>
2023-11-03 22:51:25 +0800

    tbc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,26 @@
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { createTheme } from "@mui/material/styles"
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
   <div className="app">
+    <BrowserRouter>
   <ThemeProvider theme={theme}>
     <CssBaseline/>
-    hi
+    <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+    <Routes>
+      <Route path="/" element={<div>Dashboard Page</div>}/>
+      <Route path="/predictions" element={<div>precision Page</div>}/>
+    </Routes>
+    </Box>
   </ThemeProvider>
+  </BrowserRouter>
   </div>
   );
 }
-
+// todo: navbar
 export default App
