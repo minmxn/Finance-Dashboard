d2bdf77fd3a9eb51abf20638079ae852b0587bf8
minmxn <72003734+minmxn@users.noreply.github.com>
2023-12-03 22:44:04 +0800

    next step - create mongodb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { createTheme } from "@mui/material/styles"
 import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "@/scenes/navbar"
+import Dashboard from "@/scenes/dashboard"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -15,8 +16,8 @@ function App() {
     <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
       <Navbar></Navbar>
     <Routes>
-      <Route path="/" element={<div>Dashboard Page</div>}/>
-      <Route path="/predictions" element={<div>precision Page</div>}/>
+      <Route path="/" element={<Dashboard/>}/>
+      <Route path="/predictions" element={<div>predictions page</div>}/>
     </Routes>
     </Box>
   </ThemeProvider>
@@ -24,5 +25,5 @@ function App() {
   </div>
   );
 }
-// todo: navbar
+
 export default App
